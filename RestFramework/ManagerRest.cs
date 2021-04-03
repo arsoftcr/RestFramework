d246b3b26e677f46e935817b70da3eba5bd5cb06
richard <rviquez@softland.cr>
2021-04-03 00:33:32 -0600

    rest

---
+++ RestFramework/ManagerRest.cs
@@ -0,0 +1,79 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestFramework
+{
+    public class ManagerRest
+    {
+
+        private static RestClient cliente;
+
+        private static RestRequest solicitud;
+
+        private static IRestResponse respuesta;
+
+        public static RestClient Cliente { get => cliente; set => cliente = value; }
+        public static RestRequest Solicitud { get => solicitud; set => solicitud = value; }
+        public static IRestResponse Respuesta { get => respuesta; set => respuesta = value; }
+
+        public static string Call(string url, Method metodo,
+            List<RestSharp.Parameter> parameters = null, List<RestSharp.HttpHeader> headers = null)
+        {
+            Respuesta = null;
+
+            string contenido = "";
+
+            try
+            {
+                Cliente = new RestClient(url);
+
+                Solicitud = new RestRequest(metodo);
+
+
+                if ((headers ?? new List<HttpHeader>()).Count > 0)
+                {
+
+                    foreach (var item in headers)
+                    {
+                        Solicitud.AddHeader(item.Name, item.Value);
+                    }
+
+
+                }
+
+
+                if ((parameters ?? new List<Parameter>()).Count > 0)
+                {
+
+                    foreach (var item in parameters)
+                    {
+                        Solicitud.AddParameter(item.Name, item.Value, item.Type);
+                    }
+
+
+                }
+
+                Respuesta = Cliente.Execute(Solicitud);
+
+                contenido = Respuesta != null ? Respuesta.Content : "sin respuesta";
+
+            }
+            catch (Exception e)
+            {
+
+                contenido = "error";
+            }
+
+
+
+            return contenido;
+
+        }
+
+
+
+
+    }
+}
---
+++ RestFramework/Rest.cs
@@ -0,0 +1,90 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestFramework
+{
+    public class Rest : IDisposable
+    {
+
+        public static List<HttpHeader> HttpHeaders { get; set; }
+        public static List<Parameter> Parameters { get; set; }
+
+        public async Task<string> Data(string purl, Method method,
+            string json = "", object param = null,
+            List<HttpHeader> pHttpHeaders = null, List<Parameter> pParameters = null)
+        {
+            string ok = "";
+
+            string payload = string.Empty;
+
+            await Task.Run(() =>
+            {
+                bool paramete = false;
+                bool header = false;
+                bool jsonreq = false;
+                if (!string.IsNullOrWhiteSpace(json))
+                {
+                    jsonreq = true;
+                }
+                if (pParameters != null)
+                {
+                    paramete = true;
+                }
+                if (pHttpHeaders != null)
+                {
+                    header = true;
+                }
+                if (jsonreq && paramete && header)
+                {
+                    payload = ManagerRest.Call($"{purl}",
+                            method, pParameters, pHttpHeaders);
+                }
+                if (!jsonreq && !paramete && !header)
+                {
+                    payload = ManagerRest.Call($"{purl}",
+                           method, null, null);
+                }
+                if (!jsonreq && paramete && header)
+                {
+                    payload = ManagerRest.Call($"{purl}",
+                         method, pParameters, pHttpHeaders);
+                }
+                if (jsonreq && !paramete && header)
+                {
+                    payload = ManagerRest.Call($"{purl}",
+                         method, pParameters, pHttpHeaders);
+                }
+                if (jsonreq && paramete && !header)
+                {
+                    payload = ManagerRest.Call($"{purl}",
+                         method, pParameters, null);
+                }
+                if (!jsonreq && !paramete && header)
+                {
+                    payload = ManagerRest.Call($"{purl}",
+                         method, null, pHttpHeaders);
+                }
+                if (jsonreq && !paramete && !header)
+                {
+                    payload = ManagerRest.Call($"{purl}",
+                         method, pParameters, null);
+                }
+
+                ok = payload;
+
+            });
+
+
+            return ok;
+        }
+
+        public void Dispose()
+        {
+            HttpHeaders = null;
+            Parameters = null;
+        }
+    }
+}
